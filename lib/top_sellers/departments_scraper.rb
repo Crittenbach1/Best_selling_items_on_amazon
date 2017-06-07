2459b0cc17205ed4ec58ce537a6949895913804d
Cynthia Rittenbach <crittenbach@gmail.com>
2017-06-07 00:21:00 +0000

    required nokogiri, modified self.scrape, added initialize

--- lib/top_sellers/departments_scraper.rb
+++ lib/top_sellers/departments_scraper.rb
@@ -1,42 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
 class TopSellers::Departments
+
+
       def self.scrape
 
-          puts "
-          1. Amazon Launchpad
-          2. Appliances
-          3. Apps & Games
-          4. Arts, Crafts & Sewing
-          5. Automotive
-          6. Baby
-          7. Beauty & Personal Care
-          8. Books
-          9. CDs & Vinyl
-          10. Camera & Photo
-          11. Cell Phones & Accessories
-          12. Clothing, Shoes & Jewelry
-          13. Collectible Coins
-          14. Computers & Accessories
-          15. Digital Music
-          16. Electronics
-          17. Entertainment Collectibles
-          18. Gift Cards
-          19. Grocery & Gourmet Food
-          20. Health & Household
-          21. Home & Kitchen
-          22. Industrial & Scientific
-          23. Kindle Store
-          24. Kitchen & Dining
-          25. Magazine Subscriptions
-          26. Movies & TV
-          27. Musical Instruments
-          28. Office Products
-          30. Prime Pantry
-          31. Software
-          32. Sports & Outdoors
-          33. Sports Collectibles
-          34. Tools & Home Improvement
-          35. Toys & Games
-          36. Video Games
-          37. Pet Supplies"
+
+      end
+
+    def initialize
+      @departments = self.scrape
+
     end
+
+
+
 end
