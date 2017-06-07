5bbd4fd2b98902681a924aa650d95420dcacb6fb
Cynthia Rittenbach <crittenbach@gmail.com>
2017-06-07 01:21:59 +0000

    scraped amazon departments page list to a hash with url +nae

--- lib/top_sellers/departments_scraper.rb
+++ lib/top_sellers/departments_scraper.rb
@@ -1,12 +1,26 @@
 require 'nokogiri'
 require 'open-uri'
 class TopSellers::Departments
+      attr_accessor :departments
 
-
-      def self.scrape
-
-
+      def scrape
+        folder = {}
+       html = Nokogiri::HTML(open('https://www.amazon.com/Best-Sellers/zgbs/ref=zg_bs_tab'))
+            i = 1
+            html.css('ul#zg_browseRoot ul li').each do |cat|
+              folder[i] = []
+              folder[i][0] = cat.text
+              folder[i][1] = cat.css("a").attribute("href").value
+              i += 1
+            end
+        puts folder
       end
+
+      def list_departments
+        puts @departments[1][0]
+      end
+
+
 
     def initialize
       @departments = self.scrape
