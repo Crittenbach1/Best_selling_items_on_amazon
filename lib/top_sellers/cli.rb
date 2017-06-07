97332bc76d575c709fb87f0c5681b6066618585d
Cynthia Rittenbach <crittenbach@gmail.com>
2017-06-07 01:56:38 +0000

    added list_category method

--- lib/top_sellers/cli.rb
+++ lib/top_sellers/cli.rb
@@ -1,15 +1,20 @@
 
 class TopSellers::CLI
+  attr_accessor :amazon
 
    def call
      list_categories
-     puts "Pick a categoriy number to view Top Sellers or type exit."
+     list_category
    end
 
    def list_categories
      puts "Amazon Departments:"
-     amazon = TopSellers::Departments.new
-
+     @amazon = TopSellers::Departments.new
    end
+
+   def list_category
+      puts "Pick a categoriy number to view Top Sellers or type exit."
+   end
+
 
 end
